0a37a90a00cfd04612360cfae12c1e69a7bdb454
joe beuckman <joebeuckman@gmail.com>
2014-12-14 20:17:30 -0600

    conforming to the schematic

--- MIDI_CV/MIDI_CV.ino
+++ MIDI_CV/MIDI_CV.ino
@@ -2,6 +2,11 @@
 #include "AH_MCP4922.h"
 
 #define LED 13   		    // LED pin on Arduino Uno
+#define Gate1 1
+#define Gate2 2
+#define Gate3 3
+#define Gate4 4
+
 
 //define AnalogOutput (MOSI_pin, SCK_pin, CS_pin, DAC_x, GAIN) 
 
@@ -15,36 +20,65 @@ int liveNoteCount = 0;
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
+byte selectedChannel = 17;
 
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
+  if (selectedChannel == 17) {
+    selectedChannel = channel;
+  }
+  else if (channel != selectedChannel) {
+    return;
+  }
+  
   liveNoteCount++;
   
   AnalogOutput1.setValue((pitch - 12) * 42);
-  AnalogOutput2.setValue(velocity * 32);
+  AnalogOutput3.setValue(velocity * 32);
 
   digitalWrite(LED,HIGH);
+/*  
+    digitalWrite(Gate1,HIGH);
+    digitalWrite(Gate2,HIGH);
+    digitalWrite(Gate3,HIGH);
+    digitalWrite(Gate4,HIGH);
+    */
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
+  if (channel != selectedChannel) {
+    return;
+  }
   liveNoteCount--;
   
   if (liveNoteCount == 0) {
     digitalWrite(LED,LOW);
+/*    
+    digitalWrite(Gate1,LOW);
+    digitalWrite(Gate2,LOW);
+    digitalWrite(Gate3,LOW);
+    digitalWrite(Gate4,LOW);
+    */
   }
 }
 
 void handleControlChange(byte channel, byte number, byte value)
 {
+  if (channel != selectedChannel) {
+    return;
+  }
   if (number == 1) {
-    AnalogOutput3.setValue(value * 32);
+    AnalogOutput4.setValue(value * 32);
   }
 }
 
 void handlePitchBend(byte channel, int bend)
 {
-    AnalogOutput4.setValue(bend/4.0 + 2048.0);
+  if (channel != selectedChannel) {
+    return;
+  }
+    AnalogOutput2.setValue((float)bend/4.0 + 2048.0);
 }
 
 
@@ -53,6 +87,10 @@ void handlePitchBend(byte channel, int bend)
 void setup()
 {
     pinMode(LED, OUTPUT);
+    pinMode(Gate1, OUTPUT);
+    pinMode(Gate2, OUTPUT);
+    pinMode(Gate3, OUTPUT);
+    pinMode(Gate4, OUTPUT);
     
     // Connect the handleNoteOn function to the library,
     // so it is called upon reception of a NoteOn.
@@ -68,7 +106,7 @@ void setup()
     MIDI.begin(MIDI_CHANNEL_OMNI);
     
     // C8 at full velocity for calibration on powerup
-    handleNoteOn(1, 108, 127);
+    handleNoteOn(17, 108, 127);
     liveNoteCount--;
 }
 
